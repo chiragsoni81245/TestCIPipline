92d7bc6bc832718a254aed7d69a1e3fef12ff89d
Chirag Soni <chiragsoni81245@gmail.com>
2020-08-09 15:10:34 +0530

    Added .travis.yml and testcases

---
+++ tests/test_index.py
@@ -0,0 +1,16 @@
+import os
+import pytest
+import app
+
+
+@pytest.fixture
+def client():
+	app.app.config['TESTING'] = True
+	with app.app.test_client() as client:
+		yield client
+
+
+
+def test_empty_db(client):
+	rv = client.get('/')
+	assert b'V1 is hosted' in rv.data
