a0b34a9453eda76dd31846fef975ef423a9d7c9c
Chirag Soni <chiragsoni81245@gmail.com>
2020-08-10 06:25:36 +0530

    AWS Scrips Modified

--- AWSscripts/startServer.sh
+++ AWSscripts/startServer.sh
@@ -1,4 +1,4 @@
-virtualenv -p python3 /home/ubuntu/CD_Practice/venv >> /var/log/aws_custom_log/start_server.out 2>&1
-/home/ubuntu/CD_Practice/venv/bin/pip install -r /home/ubuntu/CD_Practice/requirements.txt >> /var/log/aws_custom_log/start_server.out 2>&1
-supervisorctl reload >> /var/log/aws_custom_log/start_server.out 2>&1
-supervisorctl start cd_practice >> /var/log/aws_custom_log/start_server.out 2>&1
+virtualenv -p python3 /home/ubuntu/CD_Practice/venv >> /var/log/aws_custom_log/start_server.out 
+/home/ubuntu/CD_Practice/venv/bin/pip install -r /home/ubuntu/CD_Practice/requirements.txt >> /var/log/aws_custom_log/start_server.out 
+supervisorctl reload >> /var/log/aws_custom_log/start_server.out 
+supervisorctl start cd_practice >> /var/log/aws_custom_log/start_server.out 
